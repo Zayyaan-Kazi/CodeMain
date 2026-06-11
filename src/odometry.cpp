1e2a99629923da003d558abcf753a344fc0a1fec
Zayyaan Kazi <Zayyaan.kazi@gmail.com>
2026-06-10 22:48:11 -0500

    Optimize odometry via caching

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -10,19 +10,26 @@ void odometry::updateOdometry(){
     Pose localOffset; // create temporary pose to adjust without affecting the main one
     float headingDeltaRAD = headingDelta * 3.14159 / 180.0; // convert to radians
     auto distanceDeltas = mainChassis->getRotationDeltas(); // first is the right delta and second is the rear delta
+    
+    //Optimization caching, i don't want to mess with the compiler flags because they are scary - Kazi
+    float arcChordFactor =  2.0*sinf(headingDeltaRAD/2.0); // compute once and cache
+    float invHeadingDeltaRAD =  1.0f / headingDeltaRAD; // multiplication is faster than division on CPU
+    
     if(fabsf(headingDelta) > tolerance){
-        
-        localOffset.x = 2.0*sinf(headingDeltaRAD/2.0)* ((distanceDeltas.first / headingDeltaRAD) + disR); // X(forward)
-        localOffset.y = 2.0*sinf(headingDeltaRAD/2.0)* ((distanceDeltas.second / headingDeltaRAD) + disB); // X(forward)
-
+        localOffset.x = arcChordFactor * ((distanceDeltas.first * invHeadingDeltaRAD) + disR); 
+        localOffset.y = arcChordFactor * ((distanceDeltas.second * invHeadingDeltaRAD) + disB);
     }else{
         return; // skip if rotation change is 0, this may need to be updated as we now using a holonomic
     }
+    
     //Rotate to global via Rotation Matrix (https://en.wikipedia.org/wiki/Rotation_matrix)
     Pose localOffsetRotated;
     float avgHeading = (globalPose.theta * 3.14159 / 180.0f) + headingDeltaRAD / 2.0f; // rotate by midpoint of heading for a more accurate change in heading
-    localOffsetRotated.x = localOffset.x * cosf(avgHeading) - localOffset.y * sinf(avgHeading);
-    localOffsetRotated.y = localOffset.x * sinf(avgHeading) + localOffset.y * cosf(avgHeading);
+    
+    float cosAvgHeading = cosf(avgHeading); //compute once and cache
+    float sinAvgHeading = sinf(avgHeading); // compute once and cache
+    localOffsetRotated.x = localOffset.x * cosAvgHeading - localOffset.y * sinAvgHeading;
+    localOffsetRotated.y = localOffset.x * sinAvgHeading + localOffset.y * cosAvgHeading;
     
     globalPose.x += localOffsetRotated.x;
     globalPose.y += localOffsetRotated.y;
